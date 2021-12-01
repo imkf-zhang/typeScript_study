af89302df13cda2a90cf97f7600734b92e49a9e6
imkf-zhang <15638452918@163.com>
2021-12-01 20:55:27 +0800

    ‘[功能]初视ts

---
+++ 1、firstts.ts
@@ -0,0 +1,2 @@
+let heroName:string = "libai"; //变量名 + ：+类型   表明这个变量是string类型的
+alert(heroName)
---
+++ js/1、firstts.js
@@ -0,0 +1,3 @@
+"use strict";
+var heroName = "libai"; //变量名 + ：+类型   表明这个变量是string类型的
+alert(heroName);
